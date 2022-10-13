521ca7855008f750d1feacf640122cd04883435a
Navanee <navaneekrish164@gmail.com>
2022-10-13 17:01:05 +0530

    js

--- main.js
+++ main.js
@@ -2,81 +2,93 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 =document.getElementById("password2");
+const password2 = document.getElementById("password2");
 
-form.addEventListener("submit",e =>{
+form.addEventListener("submit", e => {
     e.preventDefault();
     checkinput();
 })
 
-function checkinput(){
+email.addEventListener("keyup", () => {
+
+    if (email.value.includes("@") && email.value.includes(".")) {
+        // alert("ok")
+        email.style.backgroundColor = "green"
+    } else {
+        // alert("ok")
+        email.style.backgroundColor = "red"
+    }
+
+})
+
+function checkinput() {
     const usernamevalue = username.value.trim();
     const emailvalue = email.value.trim();
-    const passwordvalue = password.value.trim() ;
-    const password2value = password2.value.trim(); 
-    
-    if(passwordvalue==password2value){
-        if(passwordvalue.length > 7){
-            if(passwordin(passwordvalue)){
+    const passwordvalue = password.value.trim();
+    const password2value = password2.value.trim();
+
+
+
+    if (passwordvalue == password2value) {
+        if (passwordvalue.length > 7) {
+            if (passwordin(passwordvalue)) {
                 alert("validation sucess")
-            }else{
+            } else {
                 alert("not valid")
             }
-        }else{
+        } else {
             alert("password requires atleast 8 character")
         }
-    }else{
+    } else {
         alert("passwords does't match")
     }
-     
+
 }
 
-function passwordin(password){
-    if(password.length > 7){
-        let letter ="abcdefghijklmnopqrstuvwxyz"
-        // console.log(letter);
-        let alpepet =letter+letter.toUpperCase();
-        // console.log(alpepet);
-        let num ="0123456789";
-        // console.log(num);
-        let spl ="!@#$%^&*?/.,-_";
-        // console.log(spl);
+function passwordin(password) {
 
-        let letur =false;
-        let number =false;
-        let splchar=false;
+    let letter = "abcdefghijklmnopqrstuvwxyz"
+    // console.log(letter);
+    let alpepet = letter + letter.toUpperCase();
+    // console.log(alpepet);
+    let num = "0123456789";
+    // console.log(num);
+    let spl = "!@#$%^&*?/.,-_";
+    // console.log(spl);
 
-        for(let char of password){
-            if(letur==false){
-                if(alpepet.includes(char)){
-                    letur=true;
-                }
+    let letur = false;
+    let number = false;
+    let splchar = false;
+
+    for (let char of password) {
+        if (letur == false) {
+            if (alpepet.includes(char)) {
+                console.log(letur = true);
             }
-
-            if(number==false){
-                if(num.includes(char)){
-                    number=true;
-                }
-            }
-
-            if(splchar==false){
-                if(spl.includes(char)){
-                    splchar=true;
-                }
-            }
-
         }
 
-        if(letur && number && splchar){
-            return true
-        }else{
-            return false
+        if (number == false) {
+            if (num.includes(char)) {
+                number = true;
+            }
         }
-    
-    }else{
+
+        if (splchar == false) {
+            if (spl.includes(char)) {
+                splchar = true;
+            }
+        }
+
+    }
+
+    if (letur && number && splchar) {
+        return true
+    } else {
         return false
     }
+
 }
 
-// console.log(passwordin("huihui1"));
+
+passwordin();
 
